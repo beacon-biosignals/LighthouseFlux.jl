ee2fe7a4c192c748ff1f62dab0d447796ec0ef67
kolia <kolia.sadeghi@beacon.bio>
2020-07-29 01:42:25 +0000

    Remove probable blocker.
    `rmprocs()` can hang beyond timeout.

--- src/distributed.jl
+++ src/distributed.jl
@@ -97,7 +97,7 @@ function loss_and_gradient(model, logger::RemoteChannel)
     return (train_loss, [gradients[p] for p in gradients.params])
 end
 
-function loss_and_gradient(classifier::DistributedFluxClassifier, weights, b, logger::RemoteChannel; timeout_secs=120.0)
+function loss_and_gradient(classifier::DistributedFluxClassifier, weights, b, logger::RemoteChannel; timeout_secs=20.0)
     shards = Dict{Int,Any}( p => (loss_and_gradient, classifier.model.model, logger) for p in classifier.workerpool.workers)
     return_channel = remotecall_fetch_all(shards)
     train_loss, gradients, count = nothing, nothing, 0.0
@@ -117,17 +117,13 @@ function loss_and_gradient(classifier::DistributedFluxClassifier, weights, b, lo
             end
         else
             @warn "worker w/ pid $pid unresponsive, removing from worker pool, continuing tick without its batch data."
-            try
-                rmprocs(pid; waitfor=10)
-            catch
-                nothing
-            end
             classifier.workerpool.workers = setdiff(classifier.workerpool.workers, Set(pid))
             @async try
                 rmprocs(pid; waitfor=1)
             catch
                 rmprocs(pid; waitfor=1)
                 nothing
+                # XXX call ClusterMangers kill
             end
         end
     end
