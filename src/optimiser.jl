e35b55330ca51d0d98b2d7f662265ffa46f410e8
kolia <kolia.sadeghi@beacon.bio>
2020-08-18 21:44:58 +0000

    Bugfix: apply! needs to be in module Flux.Optimise

--- src/optimiser.jl
+++ src/optimiser.jl
@@ -9,7 +9,7 @@ end
 
 Renormalize(;m=0.1, M=1000.0) = Renormalize(m, M)
 
-function apply!(o::Renormalize, x, Δ)
+function Flux.Optimise.apply!(o::Renormalize, x, Δ)
     @. Δ = Δ * max(o.m, min(o.M, norm(x))) / norm(Δ)
     return Δ
 end
